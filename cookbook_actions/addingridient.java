e133357242a26e84ea56df47d9785d61e3e5b228
paoloespiritu <paoloespiritu@outlook.com>
2021-03-27 13:16:20 +0000

    adding cookbook actions

---
+++ cookbook_actions/addingridient.java
@@ -0,0 +1,8 @@
+package cookbook_actions;
+
+public class addingridient {
+    /**
+     * addingridient
+     * parameter: 1 long string? split with delimeter?
+     */
+}
---
+++ cookbook_actions/addrecipe.java
@@ -0,0 +1,11 @@
+package cookbook_actions;
+
+public class addrecipe {
+    /**
+     * addrecipe
+     * input: string 
+     * action: concat string to query then
+     *         execute the java
+     */
+
+}
